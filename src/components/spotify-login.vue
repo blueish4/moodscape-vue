ff9075a3d378c35befe3bf4c0dee6228eecf3b22
Michael Hathaway <michael@hathaways.me.uk>
2019-10-19 14:40:10 +0100

    scaffold

---
+++ src/components/spotify-login.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="login">Log in with spotify</button>
+</template>
+
+<script>
+import store from '../store'
+export default {
+    data: function() {
+        return {}
+    },
+    methods: {
+        login: function(){
+            console.log("do implicit grant here")
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
