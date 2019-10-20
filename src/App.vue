251ffb09d3cebec236beba0a27634af62f15e9b5
blueish <blueish4@users.noreply.github.com>
2019-10-20 10:50:06 +0100

    Try to solve race condition(?) in last commit

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <spotifyLogin clientId="fcbd8cb9249b4fb28c041a433bdccd07"/>
     <div :v-if="songs">
-      <div v-for="s in songs" v-bind:key="s" id="flowers">
+      <div v-for="s in songs" v-bind:key="s.id" class="flowers">
         <song :id="s.id" />
       </div>
     </div>
@@ -20,9 +20,16 @@ export default {
     spotifyLogin,
     song
   },
-  data: function () {
-    return {
-      songs: this.$store.state.spotify.songs
+  computed: {
+    songs: function() {
+      return this.$store.state.spotify.songs
+    },
+    access_token: function() {
+      if (this.$store.state.spotify.access_token) {
+        this.$store.dispatch('loadUserData')
+        this.$store.dispatch('loadUserSongs')
+      }
+      return this.$store.state.spotify.access_token
     }
   }
 }
@@ -37,7 +44,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-#flowers {
+.flowers {
   display: inline-block
 }
 </style>
--- src/components/spotify-login.vue
+++ src/components/spotify-login.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div v-if="this.$data.access_token">
+    <div v-if="this.access_token">
       <p> Welcome, {{ name }}</p>
-      <p>{{ this.$store.state.spotify.access_token }}</p>
+      <p>{{ this.access_token }}</p>
       <button @click="wipeData">Log out</button>
     </div>
     <div v-else>
@@ -13,24 +13,35 @@
 </template>
 
 <script>
-import axios from 'axios'
 export default {
   props: {
     clientId: String
   },
   data: function() {
     const tokenParams = this.getHashParams()
-    let name = 'moodscape'
-    this.$store.dispatch('loadUserData')
-    this.$store.dispatch('loadUserSongs')
-    console.log(this.$store.state.spotify) // eslint-disable-line no-console
     if (tokenParams.access_token) {
       this.$store.commit('setAccessToken', tokenParams.access_token)
     }
+    if (this.$store.state.spotify.access_token) {
+      this.$store.dispatch('loadUserData')
+      this.$store.dispatch('loadUserSongs')
+    }
     return {
-      token: tokenParams,
-      access_token: this.$store.state.spotify.access_token,
-      name
+      token: tokenParams
+    }
+  },
+  computed: {
+    name: function() {
+      if(this.$store.state.spotify.user.display_name) {
+        return this.$store.state.spotify.user.display_name
+      } else {
+        return 'moodscape'
+      }
+    },
+    access_token: function() {
+      this.$store.dispatch('loadUserData')
+      this.$store.dispatch('loadUserSongs')
+      return this.$store.state.spotify.access_token
     }
   },
   methods: {
@@ -71,15 +82,6 @@ export default {
         hashParams[element[0]] = element[1]
       })
       return hashParams
-    },
-    getName: async function() {
-      debugger // eslint-disable-line no-debugger
-      const user = await axios.get('https://api.spotify.com/v1/me', {
-        headers: {
-          'Authorization': 'Bearer ' + this.$store.state.spotify.access_token
-        }
-      })
-      return user.data
     },
     showToken: function() {
       alert(this.$store.state.spotify.access_token)
